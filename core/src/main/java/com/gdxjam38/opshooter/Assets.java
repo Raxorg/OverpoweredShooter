351cea449fdb2681dae1d9d858789311bd4b60fa
Hero-premium <288237143+Hero-premium@users.noreply.github.com>
2026-09-16 18:42:03 +0300

    added the default skit assets for future use

--- core/src/main/java/com/gdxjam38/opshooter/Assets.java
+++ core/src/main/java/com/gdxjam38/opshooter/Assets.java
@@ -2,11 +2,14 @@ package com.gdxjam38.opshooter;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.scenes.scene2d.ui.Skin;
 
 public class Assets {
     public static final Texture ARROW = new Texture(Gdx.files.internal("arrow2.png"));
+    public static final Skin SKIN = new Skin(Gdx.files.internal("uiskin/uiskin.json"));
 
-    public static void dispose(){
+    public static void dispose() {
         ARROW.dispose();
+        SKIN.dispose();
     }
 }
